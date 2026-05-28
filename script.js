55171d1cfeb342cc9e166bd5a511aa41b8a42399
PTR-pt <740567418@qq.com>
2026-05-29 00:37:54 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-// 获取按钮元素
-const btn = document.getElementById('myBtn');
-// 获取显示文字的区域
-const demo = document.getElementById('demo');
+const btn1 = document.querySelector(".btn1");
+const msg = document.getElementById("msg");
 
-// 点击按钮时执行
-btn.addEventListener('click', function() {
-    demo.textContent = '✅ 按钮点击成功！网站运行正常～';
-    demo.style.color = 'green';
-    demo.style.fontSize = '18px';
+btn1.addEventListener("click", () => {
+    msg.textContent = "✅ 网站美化成功！超好看吧！";
 });
